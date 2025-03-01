f8a90812b13c1208118d11bfafb518eea9be02ae
Larissa Almeida <oa.larissa@hotmail.com>
2025-03-01 20:28:55 +0100

    Solved Lab

--- src/movies.js
+++ src/movies.js
@@ -1,25 +1,126 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
-// How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+// Iteration 1: 
+function getAllDirectors(movies) {
+    return movies.map((element) => {return element.director })
+  }
+console.log(getAllDirectors(movies))
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+// Iteration 2:
+function howManyMovies(movies) {
+    if (movies.length === 0) {
+        return 0;
+    }
+    return movies.filter((element) => {
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+       if (element.director === 'Steven Spielberg' && element.genre.includes('Drama')) {
+        return true
+       } else {
+        return false 
+       }
+    }).length /*.length serves to return a number, just like in a for loop for example*/
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+}
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+//Iteration 3:
+function scoresAverage(movies) {
+    if (movies.length === 0) {
+        return 0
+    }
+ const totalScore = movies.reduce((acc, currentElement) => {
+    if(currentElement.score) {
+        return acc + currentElement.score  
+    } else {
+        return acc 
+    }
+ }, 0);
+
+ const average = totalScore / movies.length
+ return Math.round(average * 100) / 100;
+}
+
+// Iteration 4:
+function dramaMoviesScore(movies) {
+
+const dramaMovies = movies.filter((element) => {
+    if (element.genre.includes('Drama')) {
+        return true
+    }
+})
+
+if (dramaMovies.length === 0) {
+    return 0
+}
+    
+const dramaAverage = dramaMovies.reduce((acc, currentElement) =>{
+    if (currentElement.genre.includes('Drama')) {
+        return acc + currentElement.score
+    } else {
+        return acc 
+    }
+}, 0)
+const average = dramaAverage / dramaMovies.length
+return Math.round(average * 100) / 100
+
+}
+
+
+//Iteration 5:
+function orderByYear(movies) {
+return movies.toSorted((movie1, movie2) => {
+    if (movie1.year > movie2.year) {
+        return 1
+    } else if (movie1.year< movie2.year) {
+        return -1 
+    } else {
+        if (movie1.title > movie2.title) {
+            return 1
+        } else if (movie1.title < movie2.title) {
+            return -1
+        } else {
+            return 0
+        }
+    }
+})
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(movies) {
+
+    return movies.map((element) => element.title).toSorted((movie1, movie2) =>{
+        if(movie1 > movie2) {
+            return 1
+        } else if (movie1 < movie2) {
+            return  -1
+        } else {
+            return 0
+        }
+    }).slice(0, 20)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(movies) {
+    return movies.map((element) => {
+        const durationParts = element.duration.split(' ');
+        let durationInMinutes = 0;
+
+        durationParts.forEach(currentElement => {
+            if (currentElement.includes('h')) {
+                durationInMinutes += Number(currentElement.replace('h', '')) * 60;
+            } else if (currentElement.includes('min')) {
+                durationInMinutes += Number(currentElement.replace('min', ''));
+            }
+        });
+
+        return {
+            ...element,
+            duration: durationInMinutes
+        };
+    });   
+}
+console.log(turnHoursToMinutes(movies))
+
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(movies) {
+//{?}
+
+}
